a86c7d977d1bf83cedc0052e352949dd7d1f2817
kanisenthil <124779486+kanisenthil@users.noreply.github.com>
2025-05-21 16:16:35 +0530

    mongo

--- Employee-Attendance-System-MERN/backend/server.js
+++ Employee-Attendance-System-MERN/backend/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect("mongodb://localhost:27017/DYE", {
+  .connect("mongodb+srv://aswinks22aid:Admin@123@cluster0.onnmj11.mongodb.net/DYE", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
